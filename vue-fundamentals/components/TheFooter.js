a3545841922a9072ae5db9a6a4ae8d3c98fcc767
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 12:42:27 +0100

    vue components, footer added

---
+++ vue-fundamentals/components/TheFooter.js
@@ -0,0 +1,9 @@
+app.component('the-footer', {
+  template: /*html*/`
+  <footer class="bg-dark bg-gradient">
+    <h5 class="pt-2 text-center">
+      &copy; <a href="http://javimoreno.epizy.com/#/">Javi Moreno</a> - {{ new Date().getFullYear() }}
+    </h5>
+  </footer>
+  `
+})

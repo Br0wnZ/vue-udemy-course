7838d08907a39ddd7b22185b3e4fda25f540f6ba
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 11:28:22 +0100

    disable button

--- vue-fundamentals/main.js
+++ vue-fundamentals/main.js
@@ -2,17 +2,27 @@ const app = Vue.createApp({
   data: () => ({
     title: 'My Bank',
     bankUrl: 'http://javimoreno.epizy.com/#/',
-    amount: 0,
+    amount: 100,
     status: true,
     transactions: [
       'Expenses',
       "Income",
       "Wire transfer"
-    ]
+    ],
+    alertMessage: 'Your balance is 0',
+    disabled: false
   }),
   methods: {
     addBalance() {
       this.amount += 50
-    }
+      this.disabled = false
+    },
+    decreaseBalance() {
+      this.amount <= 0 ? this.showAlert() : this.amount -= 50
+    },
+    showAlert() {
+      this.disabled = true
+      alert(this.alertMessage)
+    } 
   },
 })

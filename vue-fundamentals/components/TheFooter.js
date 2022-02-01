4c32752ccb52b7ef1c33be0143b4f74828535a74
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 12:45:33 +0100

    data on components

--- vue-fundamentals/components/TheFooter.js
+++ vue-fundamentals/components/TheFooter.js
@@ -2,8 +2,12 @@ app.component('the-footer', {
   template: /*html*/`
   <footer class="bg-dark bg-gradient">
     <h5 class="pt-2 text-center">
-      &copy; <a href="http://javimoreno.epizy.com/#/">Javi Moreno</a> - {{ new Date().getFullYear() }}
+      &copy; <a :href="url">{{ text }}</a> - {{ new Date().getFullYear() }}
     </h5>
   </footer>
-  `
+  `,
+  data: () => ({
+    text: 'Javi Moreno',
+    url: 'http://javimoreno.epizy.com/#/'
+  })
 })

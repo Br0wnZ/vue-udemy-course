317fc6d65f07d29449e852ae56b796343f983689
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 15:38:15 +0100

    parent to child component communication

--- vue-cli/hello-world/src/components/Account.vue
+++ vue-cli/hello-world/src/components/Account.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Balance {{ amount }}€</h2>
-    <h3>{{ accountStatus ? 'Active' : 'Inactive' }} Account</h3>
+    <h3>{{ accountStatus ? texts.active : texts.inactive }}</h3>
 
     <hr />
 
@@ -9,10 +9,14 @@
     <ul>
       <li v-for="(item, index) in transactions" :key="index">{{ titleCase(item) }}</li>
     </ul>
+
+    <Actions :buttonText="texts.increaseText"/>
+    <Actions :buttonText="texts.decreaseText"/>
   </div>
 </template>
 
 <script>
+import Actions from '@/components/Actions.vue'
 export default {
   data: () => ({
     amount: 1000,
@@ -22,15 +26,33 @@ export default {
       "income",
       "wire transfer"
     ],
+    texts: {
+      active: 'Active Account',
+      inactive: 'Inactive Account',
+      increaseText: '+ Increase',
+      decreaseText: '- Decrease',
+    }
   }),
+  components: {
+    Actions
+   },
   methods: {
     titleCase(text) {
       return text.toLowerCase().split(' ').map(word =>
         (word.charAt(0).toUpperCase() + word.slice(1))).join(' ')
-    }
+    },
+    increase() {
+      this.amount += 100
+    },
+    decrease() {
+      this.amount -= 100
+    },
   },
 }
 </script>
 
 <style scoped>
+  li {
+    list-style: none;
+  }
 </style>
---
+++ vue-cli/hello-world/src/components/Actions.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>{{ buttonText }} amount</button>
+</template>
+
+<script>
+  export default {
+    props: {
+      buttonText: String
+    }  
+  }
+</script>
+
+<style  scoped>
+
+</style>

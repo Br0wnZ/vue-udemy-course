512c60281bc9ef74c00a037aabcaa635dc520b72
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 11:18:43 +0100

    click event

--- vue-fundamentals/main.js
+++ vue-fundamentals/main.js
@@ -2,7 +2,17 @@ const app = Vue.createApp({
   data: () => ({
     title: 'My Bank',
     bankUrl: 'http://javimoreno.epizy.com/#/',
-    amount: 100,
-    status: true
-  })
+    amount: 0,
+    status: true,
+    transactions: [
+      'Expenses',
+      "Income",
+      "Wire transfer"
+    ]
+  }),
+  methods: {
+    addBalance() {
+      this.amount += 50
+    }
+  },
 })

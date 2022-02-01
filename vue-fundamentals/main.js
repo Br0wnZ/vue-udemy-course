10f2dc2831995edad78a01084187f4fbe081bdae
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-01 12:14:52 +0100

    computed properties and titleCase util added

--- vue-fundamentals/main.js
+++ vue-fundamentals/main.js
@@ -1,29 +1,39 @@
 const app = Vue.createApp({
   data: () => ({
-    title: 'My Bank',
+    title: 'my Bank',
     bankUrl: 'http://javimoreno.epizy.com/#/',
     amount: 100,
     status: true,
     transactions: [
-      'Expenses',
-      "Income",
-      "Wire transfer"
+      'expenses',
+      "income",
+      "wire transfer"
     ],
     alertMessage: 'Your balance is 0',
     decreaseMessage: 'You can do it this transaction. Your balance cant no be under 0.',
     disabled: false
   }),
+  computed: {
+    textColor() {
+      return this.amount > 0 ? 'text-success' : 'text-danger'
+    }
+  },
   methods: {
     addBalance(amount) {
       this.amount += amount
       this.disabled = false
     },
     decreaseBalance(amount) {
-      this.amount <= 0 
+      this.amount <= 0
         ? (this.disabled = true, this.showAlert(this.alertMessage))
-        : this.amount -= amount 
+        : this.amount -= amount
     },
-    showAlert: (message) => alert(message)
+    showAlert: (message) => alert(message),
     
+    titleCase(text) {
+      return text.toLowerCase().split(' ').map(word =>
+        (word.charAt(0).toUpperCase() + word.slice(1))).join(' ')
+    }
+
   },
 })

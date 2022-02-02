03ede91101f48454ae1e8e4d7438efe4146611b8
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-02 10:57:17 +0100

    input type checkbox

--- vue-forms/src/views/Home.vue
+++ vue-forms/src/views/Home.vue
@@ -2,7 +2,21 @@
   <div class="home">
     <form>
       <input type="text" class="form-control my-2" placeholder="Task Name" v-model="task.name" />
+
+      <div class="form-check text-start" v-for="(option, index) in checkOptions" :key="index">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :value="option.value"
+          :id="`check${index}`"
+          v-model="task.categories"
+        />
+        <label class="form-check-label" :for="`check${index}`">{{ option.name }}</label>
+      </div>
     </form>
+
+    <hr />
+
     <p>{{ task }}</p>
   </div>
 </template>
@@ -14,8 +28,13 @@ export default {
   data: () => ({
     task: {
       name: '',
-      description: ''
-    }
+      description: '',
+      categories: []
+    },
+    checkOptions: [
+      { name: 'JavaScript', value: 'javascript' },
+      { name: 'NodeJS', value: 'nodejs' },
+    ]
   }),
   components: {
   }

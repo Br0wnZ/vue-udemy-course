478db20eee2b6214cea73f1e2919f4862b682d8a
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-02 10:08:54 +0100

    v-model example for input

--- vue-vuex/src/views/About.vue
+++ vue-vuex/src/views/About.vue
@@ -2,6 +2,10 @@
   <div class="about">
     <h1>{{ title }}</h1>
     <h1>Counter: {{ counter }}</h1>
+    <hr>
+    <input type="text" v-model="text">
+    <h2>{{ text }}</h2>
+    <hr>
   </div>
 </template>
 
@@ -11,7 +15,8 @@ import { mapState } from "vuex";
 export default {
   name: 'About',
   data: () => ({
-    title: 'This is an about page'
+    title: 'This is an about page',
+    text: ''
   }),
   computed: {
     ...mapState(['counter'])

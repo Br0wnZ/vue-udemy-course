c1f034fa9e722d0468b90c18a03288a85ef300fe
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-03 10:43:27 +0100

    project created

---
+++ vue-firebase/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <TheNavbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar.vue'
+import { mapActions } from 'vuex';
+
+export default {
+  components: {
+    TheNavbar
+  },
+  created() {
+    this.loadLocalStorage()
+  },
+  methods: {
+    ...mapActions(['loadLocalStorage'])
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>

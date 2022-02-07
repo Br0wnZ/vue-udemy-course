d2e15381c2e9d840c8f02da0a1d9288e96b0b338
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-07 09:17:55 +0100

    feat(vue-composition-1): option vs composition: v-model

--- vue-composition-1/src/views/About.vue
+++ vue-composition-1/src/views/About.vue
@@ -3,6 +3,9 @@
     <h1 :style="{ 'color': color }">Counter: {{ counter }}</h1>
     <button @click="decrease">- Decrease</button>
     <button @click="increase">+ Increase</button>
+    <hr>
+    <input type="text" v-model="text">
+    <p>{{ text }}</p>
   </div>
 </template>
 
@@ -11,10 +14,11 @@ import { computed, ref } from 'vue'
 export default {
   setup() {
     const counter = ref(0)
+    const text = ref('')
     const increase = () => counter.value++
     const decrease = () => counter.value--
     const color = computed(() => counter.value < 0 ? 'red' : 'blue')
-    return { counter, increase, decrease, color }
+    return { counter, text, increase, decrease, color }
   }
 }
 </script>
--- vue-composition-1/src/views/Home.vue
+++ vue-composition-1/src/views/Home.vue
@@ -3,6 +3,9 @@
     <h1 :style="{'color' : color}">Counter: {{ counter}}</h1>
     <button @click="decrease">- Decrease</button>
     <button @click="increase">+ Increase</button>
+    <hr>
+    <input type="text" v-model="text">
+    <p>{{ text }}</p>
   </div>
 </template>
 
@@ -11,7 +14,8 @@
 export default {
   name: 'Home',
   data: () => ({
-    counter: 0
+    counter: 0,
+    text: ''
   }),
   computed: {
     color() {

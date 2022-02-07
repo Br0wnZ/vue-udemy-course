c2f64edbf6c91ddb676ec541ffc1865f1d102b06
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-07 10:08:09 +0100

    feat(vue-composition-1): option vs composition: emit

--- vue-composition-1/src/views/About.vue
+++ vue-composition-1/src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="about">
-    <Counter :counter="counter" :color="color"/>
-    <button @click="decrease">- Decrease</button>
-    <button @click="increase">+ Increase</button>
+    <Counter :counter="counter" :color="color" />
+    <!-- <button @click="decrease">- Decrease</button> -->
+    <!-- <button @click="increase">+ Increase</button> -->
+    <Button buttonText="- Decrease" @action="decrease" />
+    <Button buttonText="+ Increase" @action="increase" />
     <hr />
     <input type="text" v-model="text" />
     <p>{{ text }}</p>
@@ -11,9 +13,10 @@
 
 <script>
 import Counter from '@/components/Counter.vue'
+import Button from '@/components/Button.vue'
 import { computed, ref } from 'vue'
 export default {
-  components: { Counter },
+  components: { Counter, Button },
   setup() {
     const counter = ref(0)
     const text = ref('')

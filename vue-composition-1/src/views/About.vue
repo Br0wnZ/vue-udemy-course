166f1871b159245313fd27936ba98d7057d637f0
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-07 09:09:28 +0100

    feat(vue-composition-1): option vs composition: setup & ref

--- vue-composition-1/src/views/About.vue
+++ vue-composition-1/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="decrease">- Decrease</button>
+    <button @click="increase">+ Increase</button>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const counter = ref(0)
+    const increase = () => counter.value++
+    const decrease = () => counter.value--
+    return { counter, increase, decrease }
+  }
+}
+</script>
--- vue-composition-1/src/views/Home.vue
+++ vue-composition-1/src/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="home">
+    <h1>Counter: {{ counter}}</h1>
+    <button @click="decrease">- Decrease</button>
+    <button @click="increase">+ Increase</button>
   </div>
 </template>
 
 <script>
 
 export default {
+  name: 'Home',
+  data: () => ({
+    counter: 0
+  }),
+  methods: {
+    increase() {
+      return this.counter++
+    },
+    decrease() {
+      return this.counter--
+    },
+  },
 }
 </script>

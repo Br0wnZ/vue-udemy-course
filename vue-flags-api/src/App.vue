e5c3dbf88dff43a74383086c3e471599c384cf07
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-07 15:36:25 +0100

    feat(vue-flags-api): poject created and boostrap 5 added

---
+++ vue-flags-api/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Countries Api</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

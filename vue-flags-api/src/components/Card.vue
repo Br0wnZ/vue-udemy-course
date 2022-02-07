1c6796b90e03de19ac9b8a88c07ba50758075a50
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-07 16:46:57 +0100

    feat(vue-flags-api): displaying all countries ordered by population

---
+++ vue-flags-api/src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card text-center mb-2 my-card">
+    <div class="clas-body">
+      <h5>{{ country.name.official }}</h5>
+      <img :src="country.flags[1]" :alt="`${country.name.official} flag`" />
+      <p class="card-text">
+        <span
+          class="badge bg-secondary m-2"
+        >Capital: {{ Array.isArray(country.capital) ? country.capital[0] : country.capital }}</span>
+        <span class="badge bg-secondary m-2">Region: {{ country.region }}</span>
+        <span class="badge bg-secondary m-2">Population: {{ formatedNumber(country.population) }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['country'],
+  setup(props) {
+    const formatedNumber = (num) => new Intl.NumberFormat('de-DE').format(num)
+    return { formatedNumber }
+  }
+}
+</script>
+
+<style scoped>
+img,
+.card {
+  -webkit-box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.3);
+}
+</style>
+
--- vue-flags-api/src/components/CardList.vue
+++ vue-flags-api/src/components/CardList.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-12">{{ countries }}</div>
+      <div class="col-12" v-for="(country, index) in countries" :key="index">
+        <Card :country="country"/>
+      </div>
     </div>
   </div>
 </template>
@@ -9,7 +11,9 @@
 <script>
 import { computed, onMounted } from '@vue/runtime-core'
 import { useStore } from 'vuex'
+import Card from '@/components/Card.vue'
 export default {
+  components: { Card },
   setup(props) {
     const store = useStore()
     const countries = computed(() => store.getters.orderedCountries)

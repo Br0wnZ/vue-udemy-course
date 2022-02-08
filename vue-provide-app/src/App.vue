52c514b0b19c342c956accf99079a11cddc23317
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-08 09:37:07 +0100

    feat(vue-provide): project created

---
+++ vue-provide-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Provide & Inject</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+</style>

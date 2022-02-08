a63d47db5e4513aa90b8aeb56dc5b55c7b1eeb36
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-08 11:28:34 +0100

    feat(vue-provide): counter app with provide & inject

---
+++ vue-provide-app/src/components/CounterOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Counter: {{ counter }}</h2>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+export default {
+  setup(props) {
+    const counter = inject('counter')
+
+    return { counter }
+  }
+}
+</script>

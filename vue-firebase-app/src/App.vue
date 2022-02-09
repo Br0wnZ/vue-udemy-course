276b011cc5cdc4e7e4988b9cf0c36ce7c6f26567
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-09 12:56:17 +0100

    feat(vue-firebase-app): navbar added

--- vue-firebase-app/src/App.vue
+++ vue-firebase-app/src/App.vue
@@ -1,5 +1,17 @@
 <template>
+  <TheNavbar />
   <div class="container">
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import TheNavbar from "@/components/TheNavbar.vue";
+
+export default {
+  name: 'App',
+  components: { TheNavbar }
+}
+
+
+</script>
---
+++ vue-firebase-app/src/components/TheNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">Firestore</router-link>
+      <div class="d-flex">
+        <button class="btn btn-primary">Login</button>
+        <button class="btn btn-danger">Logout</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

1132191a659c6b890ebe7ca954d2f00262a70164
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-09 15:27:37 +0100

    feat(vue-firebase-app): vueuse added and handle user auth

--- vue-firebase-app/src/components/TheNavbar.vue
+++ vue-firebase-app/src/components/TheNavbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Firestore</router-link>
+      <router-link to="/" class="navbar-brand">{{ userName }}</router-link>
       <div class="d-flex">
-        <button @click="signIn" class="btn btn-primary">Login</button>
-        <button @click="signOut" class="btn btn-danger">Logout</button>
+        <button @click="signIn" class="btn btn-primary" v-if="!isAuthenticated">Login</button>
+        <button @click="signOut" class="btn btn-danger" v-if="isAuthenticated">Logout</button>
       </div>
     </div>
   </nav>
@@ -12,8 +12,13 @@
 
 <script setup lang="ts">
 import { useUser } from "@/composables/useUser"
+import { useAuth } from '@vueuse/firebase'
+import { computed } from "vue";
 
 const { signIn, signOut } = useUser()
+const { user, isAuthenticated } = useAuth()
+
+const userName = computed(() => isAuthenticated.value ? user?.value?.displayName : 'Todo App')
 
 </script>
 
--- vue-firebase-app/src/views/Profile.vue
+++ vue-firebase-app/src/views/Profile.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <h1>Profile</h1>
+    <pre>{{ user }}</pre>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  setup () {
-    
+<script setup lang="ts">
+import { useAuth } from '@vueuse/firebase'
 
-    return {}
-  }
-}
+const { user } = useAuth()
+
 </script>
 
 <style lang="scss" scoped>

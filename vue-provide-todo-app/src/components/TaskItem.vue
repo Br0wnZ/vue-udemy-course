39a9b7bb204f02a8ae73c030cd4f3874e05a4650
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-09 09:53:31 +0100

    feat(vue-provide-todo-app): add and display tasks list

--- vue-provide-todo-app/src/components/TaskItem.vue
+++ vue-provide-todo-app/src/components/TaskItem.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div class="alert alert-warning d-flex justify-content-between align-items-center">
+    {{ task.text }}
+    <div>
+      <i class="fas fa-check-circle text-success mx-2" role="button"></i>
+      <i class="fas fa-minus-circle text-danger" role="button"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  },
   setup() {
 
 
--- vue-provide-todo-app/src/components/TasksList.vue
+++ vue-provide-todo-app/src/components/TasksList.vue
@@ -2,7 +2,13 @@
   <div>
     <h1>Task List</h1>
     <TaskForm />
-    {{ tasks }}
+    <div class="mt-3">
+      <TaskItem v-for="(task, index) in tasks" :key="index" :task="task" />
+    </div>
+
+    <div class="alert alert-dark mt-3" v-if="!tasks.length">
+      No pending tasks 👌
+    </div>
   </div>
 </template>
 
@@ -11,12 +17,13 @@ import { ref } from '@vue/reactivity'
 import { provide } from 'vue'
 import providers from '../utils/providers';
 import TaskForm from './TaskForm.vue'
+import TaskItem from './TaskItem.vue';
 export default {
-    setup() {
-        const tasks = ref([]);
-        provide(providers.tasks, tasks)
-        return { tasks };
-    },
-    components: { TaskForm }
+  setup() {
+    const tasks = ref([]);
+    provide(providers.tasks, tasks)
+    return { tasks };
+  },
+  components: { TaskForm, TaskItem }
 }
 </script>

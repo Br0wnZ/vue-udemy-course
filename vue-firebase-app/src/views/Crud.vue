6beffa3893f895c46a9b5ade4fcdb4bbf94ce894
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-09 16:53:22 +0100

    feat(vue-firebase-app): handle and display firestore errors

--- vue-firebase-app/src/views/Crud.vue
+++ vue-firebase-app/src/views/Crud.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
-    <h1 v-if="!loading">Crud</h1>
+    <h1 v-if="!loading && !error">Crud</h1>
     <Spinner v-if="loading"/>
     <div v-if="!!todos.length">
       <pre>{{ todos }}</pre>
+    </div>
+    <div v-if="showError">
+    <Error />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Spinner from '@/components/Spinner.vue'
+import Error from '@/components/Error.vue'
 import { useUser } from "@/composables/useUser"
 import { useDB } from "@/composables/useDB";
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, provide, ref } from 'vue';
 const { isAuthenticated } = useUser()
 const { getTodos, loading } = useDB()
 const todos = ref([])
+const error = ref(null)
+const showError = computed(() => !!error.value)
+provide('error', error)
 
 onMounted(async() => {
   todos.value = await getTodos()
   if (todos.value.res) {
-    console.log(todos.value.error)
+    error.value = todos.value.error
+    return
   }
 })
 

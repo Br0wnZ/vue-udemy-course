f59187fadb55dea34e7785acaf4dabaf57f1b93a
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-09 11:04:53 +0100

    feat(vue-provide-todo-app): save and get tasks from local storage

--- vue-provide-todo-app/src/components/TasksList.vue
+++ vue-provide-todo-app/src/components/TasksList.vue
@@ -5,23 +5,26 @@
     <div class="mt-3">
       <TaskItem v-for="(task, index) in tasks" :key="index" :task="task" />
     </div>
-    <div class="alert alert-dark mt-3" v-if="!tasks.length">
-      No pending tasks 👌
-    </div>
+    <div class="alert alert-dark mt-3" v-if="!tasks.length">No pending tasks 👌</div>
   </div>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
-import { provide } from 'vue'
-import providers from '../utils/providers';
+import { provide, watchEffect } from 'vue'
+import providers from '../utils/providers'
+import storage from '../utils/storage'
 import TaskForm from './TaskForm.vue'
-import TaskItem from './TaskItem.vue';
+import TaskItem from './TaskItem.vue'
 export default {
   setup() {
-    const tasks = ref([]);
+    const tasks = ref([])
     provide(providers.tasks, tasks)
-    return { tasks };
+    if (localStorage.getItem(storage.tasks)) {
+      tasks.value = JSON.parse(localStorage.getItem(storage.tasks))
+    }
+    watchEffect(() => localStorage.setItem(storage.tasks, JSON.stringify(tasks.value)))
+    return { tasks }
   },
   components: { TaskForm, TaskItem }
 }

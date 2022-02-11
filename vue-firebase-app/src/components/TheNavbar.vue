2fc1db2c9abada98245aea8816f529bd2cf39bbf
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-11 08:38:15 +0100

    feat(vue-firebase-app): responsive navbar for mobile devices

--- vue-firebase-app/src/components/TheNavbar.vue
+++ vue-firebase-app/src/components/TheNavbar.vue
@@ -1,12 +1,34 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <router-link to="/" class="navbar-brand">{{ userName }}</router-link>
-      <div class="d-flex">
-        <router-link to="/crud" class="btn btn-primary mx-1" v-if="isAuthenticated">CRUD</router-link>
-        <router-link to="/profile" class="btn btn-primary mx-1" v-if="isAuthenticated">Profile</router-link>
-        <button @click="signIn" class="btn btn-primary" v-if="!isAuthenticated">Login</button>
-        <button @click="signOut" class="btn btn-danger mx-1" v-else>Logout</button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+        <div class="d-flex">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/crud" class="nav-link mx-1" v-if="isAuthenticated">CRUD</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/profile" class="nav-link mx-1" v-if="isAuthenticated">Profile</router-link>
+            </li>
+            <li class="nav-item">
+              <button @click="signIn" class="btn btn-outline-secondary" v-if="!isAuthenticated">Login</button>
+              <button @click="signOut" class="btn btn-outline-secondary mx-1" v-else>Logout</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>

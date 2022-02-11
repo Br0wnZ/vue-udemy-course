968b698642725f667ab303cdb9f2824e6102cee1
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-11 11:15:13 +0100

    feat(vue-firebase-app): added app image to navbar

--- vue-firebase-app/src/components/TheNavbar.vue
+++ vue-firebase-app/src/components/TheNavbar.vue
@@ -12,19 +12,26 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <router-link to="/" class="navbar-brand">{{ userName }}</router-link>
+      <router-link to="/crud" class="navbar-brand">
+        <img class="d-inline-block align-text-top" src="../../public/img/icons/favicon-32x32.png" />
+        <span class="ms-3" v-if="!isMobile">{{ userName }}</span>
+      </router-link>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
         <div class="d-flex">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link to="/crud" class="nav-link mx-1" v-if="isAuthenticated">CRUD</router-link>
+              <router-link to="/crud" class="nav-link mx-1" v-if="isAuthenticated">Todo</router-link>
             </li>
             <li class="nav-item">
               <router-link to="/profile" class="nav-link mx-1" v-if="isAuthenticated">Profile</router-link>
             </li>
             <li class="nav-item">
-              <button @click="signIn" class="btn btn-outline-secondary" v-if="!isAuthenticated">Login</button>
+              <button
+                @click="signIn"
+                class="btn btn-outline-secondary"
+                v-if="!isAuthenticated"
+              >Login</button>
               <button @click="signOut" class="btn btn-outline-secondary mx-1" v-else>Logout</button>
             </li>
           </ul>
@@ -37,10 +44,11 @@
 <script setup lang="ts">
 import { useUser } from "@/composables/useUser"
 import { useAuth } from '@vueuse/firebase'
-import { computed } from "vue";
+import { computed, inject } from "vue"
 
 const { signIn, signOut } = useUser()
 const { user, isAuthenticated } = useAuth()
+const isMobile = inject<boolean>('isMobile')
 
 const userName = computed(() => isAuthenticated.value ? user?.value?.displayName : 'Todo App')
 

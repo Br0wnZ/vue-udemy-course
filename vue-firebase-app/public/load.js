9d8e8994760dae0f3efd6f144d57039793132a16
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2022-02-14 09:01:19 +0100

    fix(vue-firebase-app): fix service worker doesnt work on production

--- vue-firebase-app/public/load.js
+++ vue-firebase-app/public/load.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production' && "serviceWorker" in navigator) {
+if (!window.location.href.includes('localhost') && "serviceWorker" in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('ServiceWorker working'))
     .catch(err => console.log(err))
